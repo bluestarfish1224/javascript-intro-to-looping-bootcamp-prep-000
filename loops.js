2855542d7b52496377fa94dd8d1345e764af27f7
loretta_okoye@hotmail.com <loretta_okoye@hotmail.com>
2018-06-04 03:43:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,14 +5,6 @@ function forLoop(array) {
       } else {      
         array.push(`I am ${i} strange loops.`)
       }  
-  }  return array
-}
-
-function forLook(array) {
-  for (var i=-; i<25; i++) {
-    if(i===1) {
-      array.push(`I am 1 strange loop.`)
-      
-    }
-  }
+  }  
+  return array
 }
